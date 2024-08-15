7c011d1b5551bd3829eaebce245a7bf50f7cbaa5
krishnakumarbalasundharam <krisgoblins@gmail.com>
2024-08-15 18:23:07 +0530

    added a movie page

---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Developed with <strong>React.js</strong> and <strong>OMDb API</strong> by <strong>Krishna Kumar</strong>.
+      </p>
+      <p>
+        Inspired by <a href="https://www.youtube.com/c/JavaScriptMastery" target="_blank" rel="noopener noreferrer">JavaScript Mastery</a>.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
